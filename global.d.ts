86473175a2b3ea44a1ef66061059ade23b8134f8
mdrijonhossainjibon <arntoken.tem@gmail.com>
2024-10-19 12:22:29 +0600

    first commit

---
+++ global.d.ts
@@ -0,0 +1,25 @@
+// telegram-web-app.d.ts
+interface TelegramUser {
+    id: number;
+    first_name: string;
+    last_name?: string;
+    username?: string;
+    language_code?: string;
+  }
+  
+  interface TelegramWebApp {
+    initData: string;
+    initDataUnsafe: {
+      user: TelegramUser;
+      query_id: string;
+      auth_date: number;
+      hash: string;
+    };
+    close: () => void;
+  }
+  
+  interface Window {
+    Telegram: {
+      WebApp: TelegramWebApp;
+    };
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in src with js, jsx, ts, tsx extensions
+  ],
+  theme: {
+    extend: {}, // You can extend the default theme here
+  },
+  plugins: [],
+}
